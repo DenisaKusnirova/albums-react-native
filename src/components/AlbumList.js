4f9b5b08e3342d139721a5abc937c4b3024d4e3d
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-04-23 12:47:02 +0200

    Create AlbumList Component that fetches the data

---
+++ src/components/AlbumList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import axios from 'axios'
+
+class AlbumList extends Component {
+    state = {
+        albums: []
+    }
+
+    componentWillMount() {
+        // Every time we make a http request with axios it returns a promise
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }))
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <Text key={album.title}>
+                {album.title}
+            </Text>
+        )
+    }
+
+    render() {
+        console.log(this.state);
+        
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        )
+    }
+}
+
+export default AlbumList
