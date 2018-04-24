c7015059378bdfb2170e310244e02a166b640199
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-04-24 09:47:45 +0200

    Link the button to Amazon

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, Linking } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 import Button from './Button'
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image} = album
+    const { title, artist, thumbnail_image, image, url} = album
     const { headerContentStyle, headerTextStyle, thumbnailStyle, thumbnailContainerStyle, imageStyle} = styles
     return (
         <Card>
@@ -28,7 +28,9 @@ const AlbumDetail = ({ album }) => {
                 />
             </CardSection>
             <CardSection>
-                <Button />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy now
+                </Button>
             </CardSection>
         </Card>
     )
